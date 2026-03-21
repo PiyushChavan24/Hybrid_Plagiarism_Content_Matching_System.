00ca21e1f7526519bd5ba87f1f6a42d9c50a530e
PiyushChavan24 <your-email-used-in-github>
2026-03-22 03:05:06 +0530

    Server update

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(
   origin: [
    "http://localhost:3000",
    "http://localhost:5173",
-   "https://hpcm-frontend.onrender.com", // add this
+   "https://hpcm-plagiarism-detection.vercel.app", // add this
   ],
   credentials: true,
  }),
